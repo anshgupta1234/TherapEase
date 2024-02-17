2450a745a5938d92e7828ac39f2575c1e7e50490
Ansh Gupta <38015438+anshgupta1234@users.noreply.github.com>
2024-02-17 11:56:43 -0800

    your mother

--- index.js
+++ index.js
@@ -44,3 +44,93 @@ var ready = loadOpenCv(OPENCV_URI);
 ready.then(function() {
   demo.init();
 });
+
+const webSocket = new WebSocket("ws://10.5.119.71:3000")
+
+webSocket.onmessage = (event) => {
+    handleSignallingData(JSON.parse(event.data))
+}
+
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "offer":
+            peerConn.setRemoteDescription(data.offer)
+            createAndSendAnswer()
+            break
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
+    }
+}
+
+function createAndSendAnswer () {
+    peerConn.createAnswer((answer) => {
+        peerConn.setLocalDescription(answer)
+        sendData({
+            type: "send_answer",
+            answer: answer
+        })
+    }, error => {
+        console.log(error)
+    })
+}
+
+function sendData(data) {
+    data.username = username
+    webSocket.send(JSON.stringify(data))
+}
+
+
+let localStream
+let peerConn
+let username
+
+username = "monkey";
+
+navigator.getUserMedia({
+    video: {
+        frameRate: 24,
+        width: {
+            min: 480, ideal: 720, max: 1280
+        },
+        aspectRatio: 1.33333
+    },
+    audio: true
+}, (stream) => {
+    localStream = stream
+    document.getElementById("webcam").srcObject = localStream
+
+    let configuration = {
+        iceServers: [
+            {
+                "urls": ["stun:stun.l.google.com:19302", 
+                "stun:stun1.l.google.com:19302", 
+                "stun:stun2.l.google.com:19302"]
+            }
+        ]
+    }
+
+    peerConn = new RTCPeerConnection(configuration)
+    peerConn.addStream(localStream)
+
+    peerConn.onaddstream = (e) => {
+        document.getElementById("remote-video")
+        .srcObject = e.stream
+    }
+
+    peerConn.onicecandidate = ((e) => {
+        if (e.candidate == null)
+            return
+        
+        sendData({
+            type: "send_candidate",
+            candidate: e.candidate
+        })
+    })
+
+    sendData({
+        type: "join_call"
+    })
+
+}, (error) => {
+    console.log(error)
+})
